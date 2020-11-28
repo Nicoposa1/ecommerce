ab7483fde804117fd861999a5bd122bcd10f5f6e
Nicolas Posa <nicoposa57@gmail.com>
2020-11-28 00:23:38 -0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `platziswag`,
+    description: `El mejor swag de platzi disponible para ti`,
+    author: `@nicoposa1`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
